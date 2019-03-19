74e20ae86d442996cca5624ce30c39f2851814ec
Haik Aslanyan <haikaslanyan@gmail.com>
2019-03-19 19:49:05 +0400

    Vector class

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "vector.hpp"
+
+int main()
+{
+    Vector<int> v;
+    for (int i = 0; i < 10; ++i) {
+        v.push_back(i);
+    }
+    v.push_back(170);
+    std::cout << v;
+    v.insert(2, 10);
+    std::cout << v;
+    v.remove(3);
+    v[8] = 111;
+    std::cout << v;
+    v.pop_back();
+    std::cout << v;
+}
